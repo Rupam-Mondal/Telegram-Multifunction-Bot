323de52fc4a9102ba95ad64066b1fbe1add1b33b
Rupam <rupammondal564@gmail.com>
2024-10-11 08:24:29 +0530

    Bug fixed

--- services/Imagefetch.js
+++ services/Imagefetch.js
@@ -6,11 +6,16 @@ dotenv.config();
 const key = process.env.ACCESS_KEY;
 
 async function Imagefetcher(object){
-    const images = await axios.get(`https://api.unsplash.com/search/photos?query=${object}&client_id=${key}`)
-    const i = Math.floor(Math.random() * 10);
-    const imageurl = images.data.results[i].urls.raw;
-    return imageurl;
+    try{
+        const images = await axios.get(`https://api.unsplash.com/search/photos?query=${object}&client_id=${key}`)
+        const i = Math.floor(Math.random() * 10);
+        const imageurl = images.data.results[i].urls.raw;
+        return imageurl;
 
+    }
+    catch(e){
+        return "No images found";
+    }
 }
 
 module.exports = {
