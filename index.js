bc197e76b6b4528d6b9fc9c618158e313c25b01d
Rupam <rupammondal564@gmail.com>
2024-10-11 07:55:18 +0530

    Text to image feature added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Telegrambot = require('node-telegram-bot-api')
 const axios = require('axios')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const { Imagefetcher } = require('./services/Imagefetch');
 
 dotenv.config();
 
@@ -15,12 +16,13 @@ bot.onText(/\/joke/ , async function (msg){
     const joke = await axios.get('https://official-joke-api.appspot.com/random_joke');
     const setup = joke.data.setup;
     const punchline = joke.data.punchline;
-    console.log(joke)
 
     bot.sendMessage(msg.chat.id , setup);
     bot.sendMessage(msg.chat.id, punchline);
 })
 
-bot.onText(/\/images/ , async function (msg){
-    bot.sendMessage(msg.chat.id , "Images function started");
+bot.onText(/\/images\/(.+)/ , async function (msg , match){
+    const userInput = match[1];
+    const response = await Imagefetcher(userInput);
+    bot.sendMessage(msg.chat.id , response);
 })
--- services/Imagefetch.js
+++ services/Imagefetch.js
@@ -7,9 +7,12 @@ const key = process.env.ACCESS_KEY;
 
 async function Imagefetcher(object){
     const images = await axios.get(`https://api.unsplash.com/search/photos?query=${object}&client_id=${key}`)
-    const imageurl = images.data.results[0].urls.raw;
+    const i = Math.floor(Math.random() * 10);
+    const imageurl = images.data.results[i].urls.raw;
     return imageurl;
 
 }
 
-Imagefetcher('plane');
+module.exports = {
+    Imagefetcher,
+}
